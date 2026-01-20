172e9f54e54e241d25bdc0c4a1db8ed3c1f7aebc
AyoubKhaoua <mr.ayoubkhaoua@example.com>
2026-01-20 17:11:44 +0100

    commit file that i m changed

--- app/database/database.sql
+++ app/database/database.sql
@@ -1,54 +1,64 @@
+-- Active: 1764683361796@@127.0.0.1@3306
+-- Active: 1764683361796@@127.0.0.1@3306
 create database CareerLink;
+
 use CareerLink;
+
 create table users (
-    id int PRIMARY KEY,
-    first_name varchar (50),
-    last_name varchar (50),
-    password varchar (50)
+    id int PRIMARY KEY AUTO_INCREMENT,
+    first_name varchar(50),
+    last_name varchar(50),
+    password varchar(50)
 );
-create table role(
-    id int PRIMARY KEY,
+
+create table role (
+    id int PRIMARY KEY AUTO_INCREMENT,
     user_id int,
-    user_role varchar (50),
+    user_role varchar(50),
     FOREIGN key (user_id) REFERENCES users (id)
 );
-create table category(
-    id int PRIMARY KEY,
+
+create table category (
+    id int PRIMARY KEY AUTO_INCREMENT,
     user_id int,
-    title varchar (50),
-    FOREIGN key (user_id) REFERENCES users(id)
-    
+    title varchar(50),
+    FOREIGN key (user_id) REFERENCES users (id)
 );
-create table tags(
-    id int PRIMARY KEY,
+
+create table tags (
+    id int PRIMARY KEY AUTO_INCREMENT,
     category_id int,
-    title varchar (50),
+    title varchar(50),
     FOREIGN key (category_id) REFERENCES category (id)
 );
+
 create table user_skills (
-    id int PRIMARY KEY,
+    id int PRIMARY KEY AUTO_INCREMENT,
     user_id int,
     skill_id int,
-    FOREIGN key(skill_id) REFERENCES tags(id) 
+    FOREIGN key (skill_id) REFERENCES tags (id)
 );
-create table job_offre(
-    id int PRIMARY KEY,
+
+create table job_offre (
+    id int PRIMARY KEY AUTO_INCREMENT,
     user_id int,
-    title varchar (50),
-    description varchar (255),
+    title varchar(50),
+    description varchar(255),
     FOREIGN key (user_id) REFERENCES users (id)
 );
+
 create table job_offre_recommended (
-    id int PRIMARY key,
+    id int PRIMARY key AUTO_INCREMENT,
     job_offre_id int,
     tag_id int,
     FOREIGN key (job_offre_id) REFERENCES job_offre (id),
-    FOREIGN key (tag_id) REFERENCES tags(id)
+    FOREIGN key (tag_id) REFERENCES tags (id)
 );
-   create table condidate_apply(
-    id int PRIMARY KEY,
-     user_id int,
+
+create table condidate_apply (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    user_id int,
     job_offre_id int,
     FOREIGN key (user_id) REFERENCES users (id),
-    FOREIGN key (job_offre_id) REFERENCES  job_offre(id)
+    FOREIGN key (job_offre_id) REFERENCES job_offre (id)
 );
